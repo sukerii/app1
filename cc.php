0edc4b197289fc65354a933f2c907cbbecaa3d4f
krisna80 <allperson80@gmail.com>
2017-01-17 14:01:36 +0700

    tes
    h

--- cc.php
+++ cc.php
@@ -33,7 +33,7 @@
       // This example requires the Places library. Include the libraries=places
       // parameter when you first load the API. For example:
       // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
+//kkk
       var map;
       var infowindow;
 	  var infowindow1;
